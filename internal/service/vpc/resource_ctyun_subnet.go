b7f67cd773212dd57fc2624d4309b2c59c71f014
zhangzhh13 <zhangzhh13@chinatelecom.cn>
2025-12-25 15:02:12 +0800

    fix: subnet 兼容ImportState失败

--- internal/service/vpc/resource_ctyun_subnet.go
+++ internal/service/vpc/resource_ctyun_subnet.go
@@ -310,28 +310,28 @@ func (c *ctyunSubnet) ImportState(ctx context.Context, request resource.ImportSt
 	defer func() {
 		if err != nil {
 			title := "导入失败：" + err.Error()
-			detail := "导入命令：terraform import [配置标识].[导入配置名称] [subnetId],[project_id][region_id]"
+			detail := "导入命令：terraform import [配置标识].[导入配置名称] [subnetId],[vpcId],[project_id][region_id]"
 			response.Diagnostics.AddError(title, detail)
 		}
 	}()
 	var cfg CtyunSubnetConfig
-	var subnetId, projectID, regionId string
+	var subnetId, vpcId, projectID, regionId string
 	// 根据分隔符数量判断是否输入了regionID
-	if strings.Count(request.ID, common.ImportSeparator) == 0 {
+	if strings.Count(request.ID, common.ImportSeparator) == 1 {
 		regionId = c.meta.GetExtraIfEmpty(regionId, common.ExtraRegionId)
 		projectID = c.meta.GetExtraIfEmpty(projectID, common.ExtraProjectId)
-		err = terraform_extend.Split(request.ID, &subnetId)
+		err = terraform_extend.Split(request.ID, &subnetId, &vpcId)
 		if err != nil {
 			return
 		}
-	} else if strings.Count(request.ID, common.ImportSeparator) == 1 {
+	} else if strings.Count(request.ID, common.ImportSeparator) == 2 {
 		regionId = c.meta.GetExtraIfEmpty(regionId, common.ExtraRegionId)
-		err = terraform_extend.Split(request.ID, &subnetId, &projectID)
+		err = terraform_extend.Split(request.ID, &subnetId, &vpcId, &projectID)
 		if err != nil {
 			return
 		}
 	} else {
-		err = terraform_extend.Split(request.ID, &subnetId, &projectID, &regionId)
+		err = terraform_extend.Split(request.ID, &subnetId, &vpcId, &projectID, &regionId)
 		if err != nil {
 			return
 		}
@@ -340,12 +340,16 @@ func (c *ctyunSubnet) ImportState(ctx context.Context, request resource.ImportSt
 		err = fmt.Errorf("subnetId不能为空")
 		return
 	}
-
+	if vpcId == "" {
+		err = fmt.Errorf("vpcId不能为空")
+		return
+	}
 	if regionId == "" {
 		err = fmt.Errorf("regionID不能为空")
 		return
 	}
 	cfg.Id = types.StringValue(subnetId)
+	cfg.VpcId = types.StringValue(vpcId)
 	cfg.RegionId = types.StringValue(regionId)
 	cfg.ProjectId = types.StringValue(projectID)
 	err = c.getAndMergeSubnet(ctx, &cfg)
--- internal/service/vpc/resource_ctyun_subnet_test.go
+++ internal/service/vpc/resource_ctyun_subnet_test.go
@@ -54,7 +54,11 @@ func TestAccCtyunSubnet(t *testing.T) {
 					id := ds.ID
 					projectID := ds.Attributes["project_id"]
 					regionId := ds.Attributes["region_id"]
-					return fmt.Sprintf("%s,%s,%s", id, projectID, regionId), nil
+					vpcID := ds.Attributes["vpc_id"]
+					if id == "" || regionId == "" || vpcID == "" {
+						return "", fmt.Errorf("id or region_id os vpcID is required")
+					}
+					return fmt.Sprintf("%s,%s,%s,%s", id, vpcID, projectID, regionId), nil
 				},
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{
@@ -67,7 +71,8 @@ func TestAccCtyunSubnet(t *testing.T) {
 				ImportStateIdFunc: func(s *terraform.State) (string, error) {
 					ds := s.RootModule().Resources[resourceName].Primary
 					id := ds.ID
-					return fmt.Sprintf("%s", id), nil
+					vpcID := ds.Attributes["vpc_id"]
+					return fmt.Sprintf("%s,%s", id, vpcID), nil
 				},
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{
