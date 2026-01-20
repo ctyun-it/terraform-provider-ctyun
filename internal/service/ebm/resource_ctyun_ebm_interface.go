eaa7b6b62c9ccbdcf1593ca38cbdaaded47f9bab
zhangzhh13 <zhangzhh13@chinatelecom.cn>
2026-01-20 16:56:37 +0800

    fix: ebm  添加import脚本 提示统一化

--- internal/service/ebm/resource_ctyun_ebm_interface.go
+++ internal/service/ebm/resource_ctyun_ebm_interface.go
@@ -285,8 +285,8 @@ func (c *ctyunEbmInterface) ImportState(ctx context.Context, request resource.Im
 	var err error
 	defer func() {
 		if err != nil {
-			title := c.name + "导入失败：" + err.Error()
-			detail := "导入命令：terraform import " + c.name + ".[导入配置名称] [instance_id],[interface_id],[az_name],[region_id]"
+			title := fmt.Sprintf("%s导入失败：%s", c.name, err.Error())
+			detail := fmt.Sprintf("导入命令：terraform import %s.[导入配置名称] [instance_id],[interface_id],<az_name>,<region_id>", c.name)
 			response.Diagnostics.AddError(title, detail)
 		}
 	}()
