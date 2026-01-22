7a811f8a0e7062dd50feb4b4eb87e6142d33f04a
zhangzhh13 <zhangzhh13@chinatelecom.cn>
2026-01-22 10:48:30 +0800

    fix: prefix_list 逻辑优化

--- examples/resources/ctyun_prefixlist/import.sh
+++
@@ -1,6 +0,0 @@
-# 导入前缀列表
-#[] 标记的参数为必填参数
-#<> 标记的参数为可选参数,不填则取值环境变量值
-terraform import ctyun_prefixlist.[导入配置名称] [id],<region_id>
-# 示例
-terraform import ctyun_prefixlist.prefixlist_example 376f2f85-ff34-c4e0-4f5b-320dd427a271,bb9fdb42056f11eda1610242ac110002
